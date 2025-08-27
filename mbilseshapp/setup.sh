f3bf6347815b812066c45ca0ec0578db54b07e7f
Mbilse <mbilse001@outlook.com>
2025-08-27 09:22:46 +0800

    Upgrade Care Setup

--- mbilseshapp/setup.sh
+++ mbilseshapp/setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Hello!"
-
+sleep 0.2
 echo ""
 echo "Install App"
+sleep 0.3
 echo ""
 printf "Do you want to continue? (y/n) "
   read -r ans
@@ -11,3 +12,4 @@ printf "Do you want to continue? (y/n) "
     echo "Error Installation aborted."
     exit 0
   fi
+ echo "OK"
