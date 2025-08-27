e0bb5d671a6d033ce07ca76d30e334b607263ca3
Mbilse <mbilse001@outlook.com>
2025-08-27 09:30:05 +0800

    Upgrade Mbilse CLI setup

--- mbilseshapp/setup.sh
+++ mbilseshapp/setup.sh
@@ -6,14 +6,20 @@ echo ""
 echo "Install App"
 sleep 0.3
 echo ""
-printf "Do you want to continue? (y/n) "
+
+  printf "Do you want to continue? (y/n) "
   read cont
+  cont=${cont,,}   # 转小写
   case $cont in
-  y)
-    echo "OK"
-  ;;
-  *)
-    echo "No"
-  ;;
+    y|yes)
+      echo "OK"
+      break
+    ;;
+    n|no)
+      echo "No"
+      break
+    ;;
+    *)
+      echo "请输入 y 或 n."
+    ;;
   esac
- 
