fd537f2624baa7e654f8682335d41527f588a004
Mbilse <mbilse001@outlook.com>
2026-08-12 20:07:53 +0800

    Update setup.sh

--- mbilseshapp/setup.sh
+++ mbilseshapp/setup.sh
@@ -18,22 +18,24 @@ case "$cont" in
   y|Y|yes|YES)
     echo "Working..."
     sleep 0.5
+    echo "主命令要被安装..."
     curl -fsSL https://mbilseserv.github.io/mbilseshapp/mbilse > $PREFIX/bin/mbilse
     chmod +x $PREFIX/bin/mbilse
+    echo "主命令的链接要被设置..."
     ln -s $PREFIX/bin/mbilse $PREFIX/bin/m
     ln -s $PREFIX/bin/mbilse $PREFIX/bin/mie
     ln -s $PREFIX/bin/mbilse $PREFIX/bin/Mbilse
     ln -s $PREFIX/bin/mbilse $PREFIX/bin/MBILSE
-
+    echo "imgzip要被安装..."
     curl -fsSL https://mbilseserv.github.io/mbilseshapp/imgzip > $PREFIX/bin/imgzip
     chmod +x $PREFIX/bin/imgzip
-
+    echo "askai要被安装..."
     curl -fsSL https://mbilseserv.github.io/mbilseshapp/askai > $PREFIX/bin/askai
     chmod +x $PREFIX/bin/askai
-    
+    echo "软件包管理MLE要被安装..."
     curl -fsSL https://mbilseserv.github.io/mbilseshapp/mle > $PREFIX/bin/mle
     chmod +x $PREFIX/bin/mle
-
+    echo "MSH要被安装..."
     curl -fsSL https://mbilseserv.github.io/mbilseshapp/msh > $PREFIX/bin/msh
     chmod +x $PREFIX/bin/msh
 
