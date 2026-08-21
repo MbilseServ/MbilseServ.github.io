b721a1825c2f1a8ea550cf381947f77d8a16e79b
Mbilse <mbilse001@outlook.com>
2026-08-21 19:40:11 +0800

    Update setup.sh

--- mbilseshapp/setup.sh
+++ mbilseshapp/setup.sh
@@ -29,6 +29,9 @@ case "$cont" in
     echo "imgzip要被安装..."
     curl -fsSL https://mbilseserv.github.io/mbilseshapp/imgzip > $PREFIX/bin/imgzip
     chmod +x $PREFIX/bin/imgzip
+    echo "setup.sh要被安装..."
+    curl -fsSL https://mbilseserv.github.io/mbilseshapp/setup.sh > $PREFIX/bin/setup.sh
+    chmod +x $PREFIX/bin/setup.sh
     echo "askai要被安装..."
     curl -fsSL https://mbilseserv.github.io/mbilseshapp/askai > $PREFIX/bin/askai
     chmod +x $PREFIX/bin/askai
